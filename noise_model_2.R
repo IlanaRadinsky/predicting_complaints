45f37960ebe29ce9218466ce85b1b5bba43a48e1
Ilana Radinsky <iradinsk@mail.yu.edu>
2017-12-05 20:50:52 -0500

    Attempting to construct model

--- noise_model_2.R
+++ noise_model_2.R
@@ -9,8 +9,6 @@ library(boot)
 df <- read.csv("/home/iradinsky/project/final_noise.csv", header=TRUE) %>%
     na.omit()
 
-# First attempt at model - no cross validation
-# Using glm
 sample_size <- floor(0.80 * nrow(df))
 
 set.seed(17)
@@ -19,33 +17,16 @@ train_ind <- sample(seq_len(nrow(df)), size=sample_size)
 train <- df[train_ind, ]
 test <- df[-train_ind, ]
 
-model1 <- glm(formula = perc_noise_complaints ~ avg_temp + daylight_hours + bad_weather + hours_overcast, data = train)
-summary(model1)
-
-## Second attempt at model - cross validation
-## Using cv.glmnet
 X_train <- model.matrix(perc_noise_complaints ~ avg_temp + daylight_hours + bad_weather + hours_overcast, data = train)
 y_train <- as.matrix(train$perc_noise_complaints)
 
-model2 <- cv.glmnet(X_train, y_train)
-coef(model2, s="lambda.min")
-
 X_test <- model.matrix(perc_noise_complaints ~ avg_temp + daylight_hours + bad_weather + hours_overcast, data = test)
+y_test <- as.matrix(test$perc_noise_complaints)
 
-with_pred <-
-    test %>%
-    mutate(pred=predict(model2, newx=X_test, s="lambda.min", type="response"), perc_error = abs(pred-perc_noise_complaints)/perc_noise_complaints, sq_error = (pred-perc_noise_complaints)^2) %>%
-    select(perc_noise_complaints, pred, perc_error, everything())
+                                        # Create dataframes for OLS
+X_train_df <- train %>%
+    select(-perc_noise_complaints, -date)
 
-head(with_pred)
-mean(with_pred$sq_error)
-
-summary(with_pred)
-model2$cvm[model2$lambda==model2$lambda.min]
-
-coef(model2, s="lambda.min")
-
-## Third attempt - using glm and k-fold cross validation
 model3 <- glm(perc_noise_complaints ~ avg_temp + daylight_hours + bad_weather + hours_overcast, data = df)
 
 set.seed(17)
