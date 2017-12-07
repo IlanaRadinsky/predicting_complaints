ad438fa48800da42d11697a27c16070b4f72430e
Ilana Radinsky <iradinsk@mail.yu.edu>
2017-12-07 10:14:19 -0500

    Switching from linear to logistic regression

--- noise_model_2.R
+++ noise_model_2.R
@@ -7,7 +7,8 @@ library(ROCR)
 library(boot)
 
 df <- read.csv("/home/iradinsky/project/final_noise.csv", header=TRUE) %>%
-    na.omit()
+    na.omit() %>%
+    mutate(log_perc = log(perc_noise_complaints))
 
 sample_size <- floor(0.80 * nrow(df))
 
@@ -17,41 +18,71 @@ train_ind <- sample(seq_len(nrow(df)), size=sample_size)
 train <- df[train_ind, ]
 test <- df[-train_ind, ]
 
-X_train <- model.matrix(perc_noise_complaints ~ avg_temp + daylight_hours + bad_weather + hours_overcast, data = train)
-y_train <- as.matrix(train$perc_noise_complaints)
+X_train <- model.matrix(log_perc ~ avg_temp + daylight_hours + bad_weather + hours_overcast, data = train)
+y_train <- as.matrix(train$log_perc)
 
-X_test <- model.matrix(perc_noise_complaints ~ avg_temp + daylight_hours + bad_weather + hours_overcast, data = test)
-y_test <- as.matrix(test$perc_noise_complaints)
+X_test <- model.matrix(log_perc ~ avg_temp + daylight_hours + bad_weather + hours_overcast, data = test)
+y_test <- as.matrix(test$log_perc)
 
-                                        # Create dataframes for OLS
+## Create dataframes for OLS
 X_train_df <- train %>%
-    select(-perc_noise_complaints, -date)
+    select(-log_perc, -perc_noise_complaints, -date)
+X_test_df <- test %>%
+    select(-log_perc, -perc_noise_complaints, -date)
 
-model3 <- glm(perc_noise_complaints ~ avg_temp + daylight_hours + bad_weather + hours_overcast, data = df)
+y_train_df <- train %>%
+    select(log_perc)
+y_test_df <- test %>%
+    select(log_perc)
 
-set.seed(17)
+## OLS
+ols <- glm(log_perc ~ avg_temp + daylight_hours + bad_weather + hours_overcast, data = train)
 
-cv.mse.model3 = rep(0,5)
+y_hat_train <- predict(ols, newdata = train)
+y_hat_test <- predict(ols, newdata = test)
 
-for (i in 1:5) {
-    cv.mse.model3[i] = cv.glm(df, model3, K=5)$delta[1]
-}
+mse.ols.train <- mean((y_hat_train - train$log_perc)^2)
+mse.ols.test <- mean((y_hat_test - test$log_perc)^2)
 
-mean(cv.mse.model3)
+## LASSO
+lasso <- cv.glmnet(X_train, y_train, alpha = 1)
+y_hat_train <- predict(lasso, newx = X_train, s="lambda.min")
+y_hat_test <- predict(lasso, newx = X_test, s="lambda.min")
 
-## Fourth attempt - using glm and k-fold cross validation
-## Changed y to log(y)
-model4 <- glm(log(perc_noise_complaints) ~ avg_temp + daylight_hours + bad_weather + hours_overcast, data = df)
+mse.lasso.train <- mean((y_hat_train - y_train)^2)
+mse.lasso.test <- mean((y_hat_test - y_test)^2)
 
-set.seed(17)
+## Ridge
+ridge <- cv.glmnet(X_train, y_train, alpha = 0)
+y_hat_train <- predict(ridge, newx = X_train, s="lambda.min")
+y_hat_test <- predict(ridge, newx = X_test, s="lambda.min")
 
-cv.mse.model4 = rep(0,5)
+mse.ridge.train <- mean((y_hat_train - y_train)^2)
+mse.ridge.test <- mean((y_hat_test - y_test)^2)
 
-for (i in 1:5) {
-    cv.mse.model4[i] = cv.glm(df, model4, K=5)$delta[1]
-}
+## LASSO-Ridge
+## Ridge
+lr <- cv.glmnet(X_train, y_train, alpha = 0.5)
+y_hat_train <- predict(lr, newx = X_train, s="lambda.min")
+y_hat_test <- predict(lr, newx = X_test, s="lambda.min")
 
-mean(cv.mse.model4)
+mse.lr.train <- mean((y_hat_train - y_train)^2)
+mse.lr.test <- mean((y_hat_test - y_test)^2)
 
-## Plan for next time - look at file:///C:/MSR-DS3/coursework/week3/ML_Modeling.html
-## Copy for loop to compute mse for ols, lasso, and ridge, and find the one with the lowest mse
+## Results
+print(mse.ols.train)
+print(mse.ols.test)
+print(coef(ols))
+print(summary(ols))
+
+print(mse.lasso.train)
+print(mse.lasso.test)
+print(coef(lasso, s="lambda.min"))
+      
+print(mse.ridge.train)
+print(mse.ridge.test)
+print(coef(ridge, s="lambda.min"))
+      
+print(mse.lr.train)
+print(mse.lr.test)
+print(coef(lr, s="lambda.min"))
---
+++ noise_model_3.R
@@ -0,0 +1,86 @@
+#! /usr/bin/R
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(glmnet)
+library(ROCR)
+library(boot)
+
+df <- read.csv("/home/iradinsky/project/final_noise.csv", header=TRUE) %>%
+    na.omit()
+
+sample_size <- floor(0.80 * nrow(df))
+
+set.seed(17)
+train_ind <- sample(seq_len(nrow(df)), size=sample_size)
+
+train <- df[train_ind, ]
+test <- df[-train_ind, ]
+
+X_train <- model.matrix(perc_noise_complaints ~ avg_temp + daylight_hours + bad_weather + hours_overcast, data = train)
+y_train <- as.matrix(train$perc_noise_complaints)
+
+X_test <- model.matrix(perc_noise_complaints ~ avg_temp + daylight_hours + bad_weather + hours_overcast, data = test)
+y_test <- as.matrix(test$perc_noise_complaints)
+
+## Create dataframes for OLS
+X_train_df <- train %>%
+    select(-perc_noise_complaints, -date)
+X_test_df <- test %>%
+    select(-perc_noise_complaints, -date)
+
+y_train_df <- train %>%
+    select(perc_noise_complaints)
+y_test_df <- test %>%
+    select(perc_noise_complaints)
+
+## OLS
+ols <- glm(perc_noise_complaints ~ avg_temp + daylight_hours + bad_weather + hours_overcast, data = train, family = "binomial")
+
+y_hat_train <- predict(ols, newdata = train, type="response")
+y_hat_test <- predict(ols, newdata = test, type="response")
+
+mse.ols.train <- mean((y_hat_train - train$perc_noise_complaints)^2)
+mse.ols.test <- mean((y_hat_test - test$perc_noise_complaints)^2)
+
+## LASSO
+lasso <- cv.glmnet(X_train, y_train, alpha = 1, family="binomial")
+y_hat_train <- predict(lasso, newx = X_train, s="lambda.min", type="response")
+y_hat_test <- predict(lasso, newx = X_test, s="lambda.min", type="response")
+
+mse.lasso.train <- mean((y_hat_train - y_train)^2)
+mse.lasso.test <- mean((y_hat_test - y_test)^2)
+
+## Ridge
+ridge <- cv.glmnet(X_train, y_train, alpha = 0, family="binomial")
+y_hat_train <- predict(ridge, newx = X_train, s="lambda.min", type="response")
+y_hat_test <- predict(ridge, newx = X_test, s="lambda.min", type="response")
+
+mse.ridge.train <- mean((y_hat_train - y_train)^2)
+mse.ridge.test <- mean((y_hat_test - y_test)^2)
+
+## LASSO-Ridge
+lr <- cv.glmnet(X_train, y_train, alpha = 0.5, family="binomial")
+y_hat_train <- predict(lr, newx = X_train, s="lambda.min", type="response")
+y_hat_test <- predict(lr, newx = X_test, s="lambda.min", type="response")
+
+mse.lr.train <- mean((y_hat_train - y_train)^2)
+mse.lr.test <- mean((y_hat_test - y_test)^2)
+
+## Results
+print(mse.ols.train)
+print(mse.ols.test)
+print(coef(ols))
+print(summary(ols))
+
+print(mse.lasso.train)
+print(mse.lasso.test)
+print(coef(lasso, s="lambda.min"))
+      
+print(mse.ridge.train)
+print(mse.ridge.test)
+print(coef(ridge, s="lambda.min"))
+      
+print(mse.lr.train)
+print(mse.lr.test)
+print(coef(lr, s="lambda.min"))
